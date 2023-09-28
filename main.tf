2dfc79cc0e0e221e4d49dadc62759ff4efee5ce2
Jenkins <jenkins@ip-172-31-81-30.ec2.internal>
2023-09-28 13:11:26 +0000

    v1

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.5.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name      = "Key_pair_testing"
+  tags = {
+    Name = "Machine1FromTerraform"
+    Type = "AppServer"
+    Webserver = "Nginx"
+    managed-by = "Terraform"
+  }
+}
